9f6a914c04e7a8fb3789cd64c8d3a3bbed926cd2
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2023-02-24 04:34:04 -0300

    Filtering data to display on charts, chart js

--- src/charts/BarChart.js
+++ src/charts/BarChart.js
@@ -7,24 +7,39 @@ const BarChart = () => {
 
     const [mrfData, setMrfData] = useState([]);
 
+    const [countries, setCountries ] = useState([]);
+
     const fetchApi = async () => {
         const mrfData = await fetchData();
         setMrfData(mrfData);
         // console.log(mrfData);
-   
-        const findData = mrfData.Results.filter(element => element.Country === "GERMANY");
-        console.log(findData);
-        const findCount = findData.length;
-        console.log(findCount);
 
+        // const findData = mrfData.Results.filter(element => element.Country === "GERMANY");
+        // // console.log(findData);
+        // const findCount = findData.length;
+        // console.log(findCount);
+
+        const Countries = mrfData.Results
+            .map(dataItem => dataItem.Country) // get all media types
+            .filter((mediaType, index, array) => array.indexOf(mediaType) === index); // filter out duplicates
+
+        const counts = Countries
+            .map(mediaType => ({
+                type: mediaType,
+                count: mrfData.Results.filter(item => item.Country === mediaType).length
+            }));
+
+            setCountries(counts);
+            console.log(counts);
     };
 
     useEffect(() => {
         fetchApi();
     }, []);
 
-    //   const labels = ["January", "February", "March", "April", "May", "June"];
-    const labels = mrfData.Results?.slice(0, 6).map((item) => { return item.Country });
+    //   const labels = ["GERMANY", "February", "March", "April", "May", "June"];
+    // const labels = mrfData.Results?.slice(0, 6).map((item) => { return item.Country });
+    const labels = countries.map((item) => item.type)
 
     const data = {
         labels: labels,
@@ -33,7 +48,8 @@ const BarChart = () => {
                 label: "My First dataset",
                 backgroundColor: "rgb(255, 99, 132)",
                 borderColor: "rgb(255, 99, 132)",
-                data: [10, 5, 2, 20, 30, 45],
+                // data: [10, 5, 2, 20, 30, 45],
+                data: countries.map((item) => item.count)
             },
         ],
     };
